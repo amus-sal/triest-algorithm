e43e3a94cb0fa5caff287e6839dc3b117d835112
amus-sal <abdelrahman.saleh@volvocars.com>
2023-11-25 21:14:34 +0100

    add dataset and the improvments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from triest_baseex import TriestBase
+from triest_baseex_imp import TriestBase
 import networkx as nx
+from networkx import DiGraph
 
 def create_stream(graph):
     def stream():
@@ -9,16 +10,17 @@ def create_stream(graph):
 
 
 if __name__ == '__main__':
-    num_nodes = 10
+    num_nodes = 1000
     graph = nx.complete_graph(10)
-    graph = graph.to_undirected()
+    graph2 = nx.read_edgelist("datasets/facebook_combined.txt", comments="#", create_using=DiGraph)
+    graph2 = graph2.to_undirected()
     num_edges = len(graph.edges)
     print("full graph num edges", len(graph.edges))
     triest = TriestBase(num_nodes * 3) # num_nodes * 3 seems very effective for the clique graph
-    stream = create_stream(graph)
+    stream = create_stream(graph2)
     triangles_global, triangles_per_node = triest.calculate_triangles(stream)
 
     print("triangles_global: ", triangles_global)
-    print("networkx", sum(nx.triangles(graph).values()) / 3) # this is the actual global number of triangles estimated by networkx
+    print("networkx", sum(nx.triangles(graph2).values()) / 3) # this is the actual global number of triangles estimated by networkx
 
 
---
+++ triest_baseex_imp.py
@@ -0,0 +1,101 @@
+import random
+
+import numpy as np
+
+from graphex import Graph
+
+
+class TriestBase:
+
+    def __str__(self):
+        return f'Global_triangles_counter: {self.global_triangles_counter} \n Local triangles: {self.local_triangle_counters}'
+
+    def calculate_estimation(self):
+        x_nominator = self.timestamp * (self.timestamp-1)*(self.timestamp-2)
+        x_denominator = self.sample_size * ( self.sample_size-1 ) * (self.sample_size -2)
+
+        x_t = max(1, x_nominator /x_denominator)
+        triangles_per_node = {}
+        for k,v in self.local_triangle_counters.items():
+            triangles_per_node[k] = v * x_t
+
+        triangles_global = x_t * self.global_triangles_counter
+        return triangles_per_node, triangles_global
+
+    def __init__(self, memory_size):
+        assert memory_size > 6
+        self.graph = Graph()
+        self.sample_size = memory_size
+        self.timestamp = 0
+
+        # counters
+        self.local_triangle_counters = {}
+        self.global_triangles_counter = 0
+
+
+    def update_graph(self, edge):
+        self.graph.addNode(edge[0])
+        self.graph.addNode(edge[1])
+        self.graph.addEdge(edge)
+
+    def calculate_triangles(self, stream):
+        triangles_per_node, triangles_global = 0, {}
+        for edge in stream():
+            print(f"edge on stream: {edge}")
+            self.timestamp += 1
+            if self.sample_edge(edge, self.timestamp):
+                self.update_graph(edge)
+                self.update_counters("add", edge)
+            else:
+                print("skipping")
+            triangles_per_node, triangles_global = self.calculate_estimation()
+            print(f"timestamp: {self.timestamp}, triangles_per_node: {triangles_per_node}, triangles_global: {triangles_global}")
+            print("***************")
+        triangles_per_node, triangles_global = self.calculate_estimation()
+        return triangles_global, triangles_per_node
+
+    def sample_edge(self, edge, timestamp) -> bool:
+        current_prob = self.sample_size / timestamp
+        print("timestamp: ", timestamp)
+        if timestamp <= self.sample_size:
+             return True
+        elif random.random() <= current_prob:
+            edge_list = self.graph.getEdges()
+            edge_to_remove = random.choice(edge_list)
+            self.graph.removeEdge(edge_to_remove)
+            return True
+        else:
+            return False
+
+    def update_counters(self, operation, edge):
+        (u, v) = edge
+        print("edge to update counters: ", edge)
+        u_neighbors = self.graph.getNeighbors(u)
+        v_neighbors = self.graph.getNeighbors(v)
+        common_neighbors = u_neighbors & v_neighbors
+        print("Common neighbours: " ,common_neighbors)
+        weight = max(1,((self.timestamp-1)*(self.timestamp-2))/(self.sample_size * (self.sample_size -1)))
+        for c in common_neighbors:
+            print(f"operation {operation} on : ", c)
+            self.global_triangles_counter += weight
+            try:
+                self.local_triangle_counters[c] += weight
+            except:
+                self.local_triangle_counters[c] = 1
+            try:
+                self.local_triangle_counters[u] += weight
+            except:
+                self.local_triangle_counters[u] = 1
+            try:
+                self.local_triangle_counters[v] += weight
+            except:
+                self.local_triangle_counters[v] = 1
+                
+            # print(f"result on {c}: ", self.local_triangle_counters[c])
+            # print(f"result on {u}: ", self.local_triangle_counters[u])
+            # print(f"result on {v}: ", self.local_triangle_counters[v])
+
+        return None
+
+
+
