e20dd1904b7c7a50abf1d3cf7a7b9c21095f130f
Natassa Farmaki <natazf@gmail.com>
2023-11-25 16:50:21 +0100

    add comments

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ def create_stream(graph):
             yield edge
     return stream
 
+
 if __name__ == '__main__':
     num_nodes = 10
     graph = nx.complete_graph(10)
     graph = graph.to_undirected()
     num_edges = len(graph.edges)
     print("full graph num edges", len(graph.edges))
-    triest = TriestBase(num_nodes * 3)
+    triest = TriestBase(num_nodes * 3) # num_nodes * 3 seems very effective for the clique graph
     stream = create_stream(graph)
     triangles_global, triangles_per_node = triest.calculate_triangles(stream)
 
     print("triangles_global: ", triangles_global)
-    print("networkx", sum(nx.triangles(graph).values()) / 3)
+    print("networkx", sum(nx.triangles(graph).values()) / 3) # this is the actual global number of triangles estimated by networkx
 
 
