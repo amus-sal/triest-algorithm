ebdca1bcfb02c669fa38242a59b53e434313663d
Natassa Farmaki <natazf@gmail.com>
2023-11-26 22:24:10 +0100

    setup experiment

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from triest_baseex_imp import TriestBase
 import networkx as nx
 from networkx import DiGraph
+import numpy as np
+import pandas as pd
+from IPython.display import display
 
 def create_stream(graph):
     def stream():
@@ -14,23 +17,36 @@ if __name__ == '__main__':
     # graph = nx.complete_graph(10)
     graph2 = nx.read_edgelist("datasets/facebook_combined.txt", comments="#", create_using=DiGraph)
     graph2 = graph2.to_undirected()
-    #num_edges = len(graph.edges)
-    #print("full graph num edges", len(graph.edges))
+    actual_num_triangles = 1612010
+
+    results_per_size_average = {}
+    results_per_size_median = {}
     for size in sizes:
-        t_avg = 0
-        i = 0
-        for count in range(0,4):
-            i += 1
+        results = []
+        for count in range(0, 4):
             triest = TriestBase(size)
             stream = create_stream(graph2)
             triangles_global, triangles_per_node = triest.calculate_triangles(stream)
-            t_avg  += triangles_global 
-            print("count: ", count)
-        t_avg = t_avg/i
+            results.append(triangles_global)
+
+        average_estimation = np.mean(results)
+        median_estimation = np.median(results)
+        results_per_size_average[size] = average_estimation
+        results_per_size_median[size] = median_estimation
+
         print("----------------------------------------") 
-        print("size: ",size) 
-        print("average : triangles_global: ",t_avg)
-        print("networkx", sum(nx.triangles(graph2).values()) / 3) # this is the actual global number of triangles estimated by networkx
-        print("The difference is :", (abs(t_avg - 1612010)))
+        print("size: ", size)
+        print("average : triangles_global: ", average_estimation)
+        print(f"median estimation: {median_estimation}")
+        # print("networkx", sum(nx.triangles(graph2).values()) / 3) # this is the actual global number of triangles estimated by networkx
+        print("Median diff :", (abs(average_estimation - actual_num_triangles)))
+        print("Median diff :", (abs(median_estimation - actual_num_triangles)))
  
 
+        average_df = pd.Dataframe(results_per_size_average)
+        median_df = pd.Dataframe(results_per_size_median)
+
+        print("**************** average ****************")
+        display(average_df)
+        print("**************** median ****************")
+        display(median_df)
